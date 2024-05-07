9d2249a08ebcaea965296f02c0bf26264e785867
AdepuShashank <shahsank.adepu5@gmail.com>
2024-05-07 10:01:50 +0530

    dbconnect changed

--- dbconnection.js
+++ dbconnection.js
@@ -1,5 +1,9 @@
 const mongoose = require("mongoose");
-const DB = "mongodb+srv://shashankadepu:Shashi@8977@cluster.e8evcrr.mongodb.net/";
+// const DB = "mongodb+srv://shashankadepu:Shashi@8977@cluster.e8evcrr.mongodb.net/";
+
+const DB =  "https://stock-price-checker-proxy.freecodecamp.rocks/v1/stock/[symbol]/quote";
+
+
 const db = mongoose.connect(process.env.DB,{
     useUnifiedTopology: true,
     useNewUrlParser: true,
